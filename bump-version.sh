1f38a8886dda236f2320c2ec3f943c3d831a27f6
andou <antonio.pastorino@gmail.com>
2016-01-29 15:36:08 +0100

    A better bump version script

--- bump-version.sh
+++ bump-version.sh
@@ -18,26 +18,11 @@ fi
 # CHECK FORMAT OF THE TAG 
 php -r "if(preg_match('/^\d+\.\d+\.\d+(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?\$/',\$argv[1])) exit(0) ;else{ echo 'format of version tag is not invalid' . PHP_EOL ; exit(1);}" $1
 
-
-# CHECK jsawk COMMAND
-command -v jsawk >/dev/null 2>&1 || { echo "Error : Command jsawk is not installed on the system"; echo "See : https://github.com/micha/jsawk "; echo  "Exiting..." >&2; exit 65; }
-
-# CHECK js COMMAND
-command -v js >/dev/null 2>&1 || { echo "Error : Command js is not installed on the system"; echo "Should be fixed by installing spidermonkey "; echo  "Exiting..." >&2; exit 65; }
-
-# CHECK box COMMAND
-command -v box >/dev/null 2>&1 || { echo "Error : Command box is not installed on the system"; echo "See : https://github.com/box-project/box2 "; echo  "Exiting..." >&2; exit 65; }
-
-# CHECK python COMMAND
-command -v python >/dev/null 2>&1 || { echo "Error : Command python is not installed on the system"; echo  "Exiting..." >&2; exit 65; }
-
 # CHECK THAT WE CAN CHANGE BRANCH
 git checkout gh-pages
 git checkout --quiet master
 
-
 TAG=$1
-
 
 #
 # Tag & build master branch
@@ -54,12 +39,12 @@ git checkout gh-pages
 cp gancinph.phar downloads/gancinph-${TAG}.phar
 git add downloads/gancinph-${TAG}.phar
 
-SHA1=$(openssl sha1 gancinph.phar)
+SHA1=$(openssl sha1 gancinph.phar | sed 's/^.* //')
 
-JSON='name:"gancinph.phar" '
-JSON="${JSON},sha1:\"${SHA1}\""
-JSON="${JSON},url:\"http://andou.github.io/gancinph/downloads/gancinph-${TAG}.phar\""
-JSON="${JSON},version:\"${TAG}\""
+JSON='[{"name":"gancinph.phar"'
+JSON="${JSON},\"sha1\":\"${SHA1}\""
+JSON="${JSON},\"url\":\"http://andou.github.io/gancinph/downloads/gancinph-${TAG}.phar\""
+JSON="${JSON},\"version\":\"${TAG}\""
 
 if [ -f gancinph.phar.pubkey ]; then
     cp gancinph.phar.pubkey pubkeys/gancinph-${TAG}.phar.pubkeys
@@ -67,20 +52,19 @@ if [ -f gancinph.phar.pubkey ]; then
     JSON="${JSON},publicKey:\"http://andou.github.io/gancinph/pubkeys/gancinph-${TAG}.phar.pubkey\""
 fi
 
+JSON="${JSON}}]"
+
 #
 # Update manifest
 #
-cat manifest.json | jsawk -a "this.push({${JSON}})" | python -mjson.tool > manifest.json.tmp
-mv manifest.json.tmp manifest.json
+echo "${JSON}" > manifest.json;
 git add manifest.json
-
 git commit -m "Bump version ${TAG}"
+git push origin gh-pages
 
 #
 # Go back to master
 #
 git checkout master
 
-echo "New version created. Now you should run:"
-echo "git push origin gh-pages"
-echo "git push ${TAG}"
+git push origin ${TAG}
